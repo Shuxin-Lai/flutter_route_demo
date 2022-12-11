7b60cede2b1b978edd29336cb9a2fbfb6feef298
Shuxin-Lai <shuxinlai001@gmail.com>
2022-12-11 08:25:21 +0800

    init: remove unused files

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:writer/router/router.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
-}
-
-/// The main app.
-class MyApp extends StatelessWidget {
-  /// Constructs a [MyApp]
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-    );
-  }
+  // runApp();
 }
--- lib/routes/routes_name.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:writer/views/404.dart';
-import 'package:writer/views/account.dart';
-import 'package:writer/views/home.dart';
-
-class RouteNames {
-  static const home = 'home';
-  static const account = 'account';
-}
